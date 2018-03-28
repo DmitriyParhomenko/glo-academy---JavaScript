04e48b731d3c38295289e37669f6e1e0e909d303
Dmitriy Parhomenko <dmmitriyparhimenko140@gmail.com>
2018-03-29 00:54:19 +0300

    Урок 3

--- Lesson_3/js/main.js
+++ Lesson_3/js/main.js
@@ -1,98 +1,64 @@
 // Создаём переменные
-var yourBudget;
-var nameStore;
-var time = 19;
+let money,
+				name,
+				time,
+				price
 
+function start() {
+	money = +prompt("Ваш бюджет?", '');
 
-// Присваиваем значение переменным
-yourBudget = +prompt("Ваш бюджет?", '');
-/*console.log( typeof(yourBudget));*/
+	while (isNaN(money) || money == "" || money == null ) {
+		money = +prompt("Ваш бюджет?", '');
+	}
 
-nameStore = prompt("Название вашего магазина", '');
-/*console.log( typeof(nameStore));*/
+	name = prompt("Название вашего магазина", '').toUpperCase();
+	time = 21;
+}
+
+start();
 
 // обявляем объект mainList
 mainList = {
-	yourBudget,
-	nameStore,
+	yourBudget: money,
+	nameStore: name,
 	shopGoods: [],
-	employers: {},
-	open: false
+	employers: {
+
+	},
+	open: false,
+	discount: false
 }
 
 // Присваиваем массиву значния введённые пользователем
-for (let i = 0; i < 5; i++ ) {
+function chooseGoods() {
+	for (let i = 0; i < 5; i++ ) {
 
-	let a = prompt("Какой тип товаров будем продавать?");
+		let a = prompt("Какой тип товаров будем продавать?");
 
-	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
-		console.log('Всё верно!');
-		mainList.shopGoods[i] = a;
-	} else {
+		if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+			console.log('Всё верно!');
+			mainList.shopGoods[i] = a;
+		} else {
 
-		if ( a == '') {
-			let b = confirm('Вы не ввели название товара, хотите оставить поле пустым?');
-			if (b) {
-				mainList.shopGoods[i] = '';
-			} else {
-				i--;
-				alert("Введите свой товар");
+			if ( a == '') {
+				let b = confirm('Вы не ввели название товара, хотите оставить поле пустым?');
+				if (b) {
+					mainList.shopGoods[i] = '';
+				} else {
+					i--;
+					alert("Введите свой товар");
+				}
 			}
-		}
 
+		}
 	}
 }
 
-/*var i = 0;
-while (i < 5) {
-	i++;
-	let a = prompt("Какой тип товаров будем продавать?");
-
-	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
-		console.log('Всё верно!');
-		mainList.shopGoods[i] = a;
-	} else {
-
-		if ( a == '') {
-			let b = confirm('Вы не ввели название товара, хотите оставить поле пустым?');
-			if (b) {
-				mainList.shopGoods[i] = '';
-			} else {
-				i--;
-				alert("Введите свой товар");
-			}
-		}
-
-	}
-}*/
-
-/*var i = 0;
-do {
-	i++;
-
-	let a = prompt("Какой тип товаров будем продавать?");
-
-	if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
-		console.log('Всё верно!');
-		mainList.shopGoods[i] = a;
-	} else {
-
-		if ( a == '') {
-			let b = confirm('Вы не ввели название товара, хотите оставить поле пустым?');
-			if (b) {
-				mainList.shopGoods[i] = '';
-			} else {
-				i--;
-				alert("Введите свой товар");
-			}
-		}
-
-	}
-
-} while ( i < 5 );*/
+chooseGoods();
 
 
 // Время работы магаза
+function workTime(time) {
 if (time < 0 ) {
 	console.log ("В прошлое мы не можем вернутся!");
 } else if (time > 8 && time < 20) {
@@ -102,13 +68,47 @@ if (time < 0 ) {
 		} else {
 			console.log ("В сутках только 24 часа!");
 			}
+}
 
+workTime(23);
 
 
 //  Бюджет на 1 день
-var budgetDay = yourBudget/30;
+function budget() {
+let budgetDay = money/30;
 
 alert("Бюджет на 1 день" + " " + "=" + " " + budgetDay);
 
 console.log (mainList);
+}
 
+budget();
+
+// 80% от цены 
+function priceNew() {
+
+	price = +prompt("Ваш цена?", '');
+
+	let partPrice = price*0.8;
+
+	console.log ( Math.round (partPrice) );
+}
+
+priceNew();
+
+function employers() {
+  	for (var i = 0; i < 4; i++) {
+
+  		let employer = prompt("Введите имя сотрудника");
+  		if (employer == "" || employer == null ) {
+  			alert("Нужно ввести имя сотрудника");
+  			i--;
+  		} else {
+  			  mainList.employers[++i] = employer;
+  			  i--;
+  			} 
+  	}
+
+  }
+
+  employers();
