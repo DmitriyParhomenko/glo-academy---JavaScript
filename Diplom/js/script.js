6f419d6e7bbcb73adfa4b3a1e3ea088559c70887
Dmitriy Parhomenko <dmmitriyparhimenko140@gmail.com>
2018-04-20 01:55:38 +0300

    Диплом 20.04

--- Diplom/js/script.js
+++ Diplom/js/script.js
@@ -4,19 +4,19 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	function fade(elem, t, f){
   // кадров в секунду (по умолчанию 50)
-  var fps = f || 50; 
+  let fps = f || 50; 
   // время работы анимации (по умолчанию 500мс)
-  var time = t || 500; 
+  let time = t || 500; 
   // сколько всего покажем кадров
-  var steps = time / (1000 / fps);   
+  let steps = time / (1000 / fps);   
   // текущее значение opacity - изначально 0
-  var op = 1;
+  let op = 1;
   // изменение прозрачности за 1 кадр
-  var d0 = op / steps;
+  let d0 = op / steps;
   
   // устанавливаем интервал (1000 / fps) 
   // например, 50fps -> 1000 / 50 = 20мс  
-  var timer = setInterval(function(){
+  let timer = setInterval(function(){
     // уменьшаем текущее значение opacity
     op -= d0;
     // устанавливаем opacity элементу DOM
@@ -38,35 +38,93 @@ window.addEventListener('DOMContentLoaded', function() {
 	// Модальное окно
 
 	function showCustom() {
-			let custom = document.getElementsByClassName('custom')[0]; 
-				customInfo = document.getElementsByClassName('custom-info')[0],
-				customChar = document.getElementsByClassName('custom-char')[0],
-				customStyle = document.getElementsByClassName('custom-style')[0];
-			
+			let custom = document.getElementsByClassName('custom')[0],
+				person = document.getElementsByClassName('person-easy')[0],
+				sliderPreview = document.querySelector('.preview');
+
 
 			custom.style.display = "flex";
-			customInfo.style.display = "block";
-			customChar.style.display = "block";
-			customStyle.style.display = "block";
+			person.style.cssText = "background: url(../img/candidate-1.png) center no-repeat; \
+									background-size: 200px; \
+									";
 
+			sliderPreview.style.cssText = "background: url(../img/candidate-1.png) center no-repeat; \
+										    border-radius: 50px; \
+										    background-size: 145px; \
+							    			border: solid 1px;";
+			
+			for ( let i = 0; i < custom.children.length; i++) {
+					custom.children[i].style.display = 'block';
+				}
 		}
+
+	function hideCustom() {
+			let custom = document.getElementsByClassName('custom')[0];
+
+
+			custom.style.display = "none";
+			
+			for ( let i = 0; i < custom.children.length; i++) {
+					custom.children[i].style.display = '';
+				}
+
+			animatedMain.style.display = "block";
+			animatedMain.style.opacity = "1";
+
+			
+		}
+
+
+
 
 	let popupBtn = document.getElementById('popup-btn'),
 		animatedMain = document.getElementsByClassName('main')[0],
 		animatedOverlay = document.getElementsByClassName('overlay')[0];
 		
-
 		popupBtn.addEventListener('click', function(){
-
 
 			fade(animatedMain, 1000, 50);
 			fade(animatedOverlay, 1000, 50);
-			showCustom();
+
+			setTimeout(function() {
+			  showCustom();
+			}, 1500);
 
 		});
 
-	
+
+	// Информация о кандидате
+
+	let readyBtn = document.getElementById('ready'),
+		nameInputId = document.getElementById('name'),
+		nameClass = document.getElementsByClassName('name')[0],
+		age = document.getElementById('age');
+
+		console.log(nameClass);
+		console.log(nameInputId);
+
+
+	readyBtn.addEventListener('click', () => {
 
 		
+		 if (nameInputId.value == "" || !isNaN(nameInputId.value) ) {
+			showCustom();	
+		} else {
+			nameClass.textContent = nameInputId.value;
+			hideCustom();
+		}
+
+
+		
+
+	});
 	
 });
+
+
+
+
+
+/*for ( let i = 0; i < input.length; i++) {
+	input[i].value = '';  // Очищаем поля ввода
+}*/
